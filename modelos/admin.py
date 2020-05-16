33682369f7ecdc88d1a9367227e3e0a7b37ce50a
marcos <marcosazcona2000@hotmail.com>
2020-05-16 11:13:17 -0300

    Cambiado redirect del admin luego de agregar / cambiar

--- modelos/admin.py
+++ modelos/admin.py
@@ -1,17 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from Bookflix import views
 from django import forms
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from .models import Genero,Autor,Editorial,Libro,Suscriptor,Novedad
 from django.core.exceptions import ValidationError
+from .models import Genero,Autor,Editorial,Libro,Suscriptor,Novedad
+from django.shortcuts import redirect
+from django.urls import reverse
+
 
 
 class NovedadAdmin(admin.ModelAdmin):
     list_display=('titulo',)
     list_per_page = 2
     view_only=True
+    #https://stackoverflow.com/questions/1339845/redirect-on-admin-save/1340106
+    def response_add(self, request, obj, post_url_continue=None):
+        return redirect('/listado_novedades/')
     
+    def response_change(self, request, obj, post_url_continue=None):
+        return redirect('/listado_novedades/')
+
 
 admin.site.site_header = 'Panel de Administracion Bookflix'
 
